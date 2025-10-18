13d7d7ecf781cdc6155b6d97b91c4f140d52ef9e
Pranav Rao <110892097+Rao-1301@users.noreply.github.com>
2025-10-18 01:54:15 -0500

    Update deploy.js

--- blockchain-project/scripts/deploy.js
+++ blockchain-project/scripts/deploy.js
@@ -1,13 +1,31 @@
+// const hre = require("hardhat");
+
+// async function main(){
+//     const Lock = await hre.ethers.getContractFactory("Lock");
+//     const lock = await Lock.deploy(unlockTime, {value:lockedAmount});
+
+//     await lock.deployed();
+
+//     console.log(
+//         `Tracking deployed to ${lock.address}`
+//     );
+// }
+
+// main().catch((error)=>{
+//     console.error(error);
+//     process.exitCode=1;
+// });
+
 const hre = require("hardhat");
 
 async function main(){
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, {value:lockedAmount});
+    const Tracking = await hre.ethers.getContractFactory("Tracking");
+    const tracking = await Lock.deploy();
 
-    await lock.deployed();
+    await tracking.deployed();
 
     console.log(
-        `Tracking deployed to ${lock.address}`
+        `Tracking deployed to ${tracking.address}`
     );
 }
 
